efebfc119d49877d992b9608aa4fa4fdc5d3c78e
Varun <bhat.varun.25@gmail.com>
2024-11-15 00:05:17 +0530

    numpy basics

--- main.py
+++ main.py
@@ -10,7 +10,18 @@ arr_2d = np.array([[1, 2, 3], [4, 5, 6]])
 print(arr_2d)
 
 # Check the dimensions of array
+# Syntax: array_name.ndim
 print(arr_2d.ndim)
+
+# Print the shape of array
+# Syntax: array_name.shape
+print(arr_2d.shape)
+
+# Creating a array filled with 0's
+# Syntax: array_name = numpy.zeros((array shape))
+zeros_arr = np.zeros((4, 5))
+print(zeros_arr)
+
 
 
 
