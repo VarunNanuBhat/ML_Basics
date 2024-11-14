b8fcfb2a90c31ff3ed0b23aa6afeb8ff1c731ccf
Varun <bhat.varun.25@gmail.com>
2024-11-14 23:30:55 +0530

    numpy basics

---
+++ main.py
@@ -0,0 +1,14 @@
+# Import numpy
+import numpy as np
+
+# Creating a basic numpy array
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
+
+# Creating a 2-D array
+arr_2d = np.array([[1, 2, 3], [4, 5, 6]])
+print(arr_2d)
+
+# Check the dimensions of array
+print(arr_2d.ndim)
+
