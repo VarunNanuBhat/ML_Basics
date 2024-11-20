dcfec6f2f716da4978e91b05521a5f032c583887
Varun <bhat.varun.25@gmail.com>
2024-11-20 22:32:59 +0530

    Cleaning data set

--- Getting_started_with_data_cleaning.py
+++ Getting_started_with_data_cleaning.py
@@ -1,5 +1,12 @@
 # Import Pandas library
 import pandas as pd
 # load CSV data into dataframe
-dataframe = pd.read_csv('data.csv')
+df = pd.read_csv('data.csv')
+# Info method can be used to get the high level information
+print(df.info())
 
+# describe method can be used to get statistical information
+print(df.describe())
+
+# Get the statistical information for all the columns
+print(df.describe(include="all"))
