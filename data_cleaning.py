443db63703c638821d0aa1624a373eea767559ad
Varun <bhat.varun.25@gmail.com>
2024-11-21 13:57:23 +0530

    Cleaning data set

--- data_cleaning.py
+++ data_cleaning.py
@@ -9,9 +9,14 @@ print(df.columns)
 df.columns = df.columns.str.lower().str.replace(' ', '_')
 # Print column names after changing
 print(df.columns)
-
-Set
-
+# set datatype for column
+df['usn'] = df['usn'].astype(str)
+# Ensure column contains only numerical data
+# Print data frame with string values in sub_1_marks column
+print(df)
+df['sub_1_marks'] = pd.to_numeric(df['sub_1_marks'], errors='coerce')
+# The string values should be replaced with Null values now.
+print(df)
 
 # Handling null values using isnull()
 # isnull() will return True/False at each position.
@@ -20,5 +25,48 @@ print(df.isnull())
 print(df.isnull().sum())
 # isnull().any() will return boolean True/False if null values are present.
 print(df.isnull().any())
+# isna() method is similar to isnull()
+print(df.isna())
 
-print(df.isna().any())
+# Filling the missing values
+# Replace missing values with fixed/default value
+#df['sub_1_marks'] = df['sub_1_marks'].fillna(0)
+#print(df)
+# Replace missing value with mean i.e., average value of column
+#df['sub_1_marks'] = df['sub_1_marks'].fillna(df['sub_1_marks'].mean())
+#print(df)
+# Replace missing value with median i.e., middle value of column
+#df['sub_1_marks'] = df['sub_1_marks'].fillna(df['sub_1_marks'].median())
+#print(df)
+# Replace missing value with mode i.e., most common value of column
+# mode()[0] accesses the first mode value if there are more than 1 modes.
+#df['sub_1_marks'] = df['sub_1_marks'].fillna(df['sub_1_marks'].mode()[0])
+#print(df)
+# Forward Fill: Use the last valid value to fill missing data.
+#df['sub_1_marks'] = df['sub_1_marks'].fillna(method='ffill')
+#print(df)
+#Backward Fill: Use the next valid value to fill missing data.
+df['sub_1_marks'] = df['sub_1_marks'].fillna(method='bfill')
+#print(df)
+
+# Removing duplicates
+# get the total no of duplicate rows
+print(df.duplicated().sum())
+
+# Handling outliers
+# Detect outliers
+# import matplotlib for plotting graph data
+import matplotlib
+# For detecting outliers we need to plot a graph using boxplot to display spread of data
+df[['sub_1_marks']].boxplot()
+matplotlib.pyplot.show()
+
+
+# Fix the outliers with max cap value as 100
+df['sub_1_marks'] = df['sub_1_marks'].clip(upper=100)
+df[['sub_1_marks']].boxplot()
+matplotlib.pyplot.show()
+print(df)
+
+
+
